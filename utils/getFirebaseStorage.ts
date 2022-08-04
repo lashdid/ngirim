d7f6d9f20317fbb8561ff86583c7fe5212c9e7d4
lashdid19 <lashdid19@gmail.com>
2022-08-04 14:32:49 +0700

    add utils

---
+++ utils/getFirebaseStorage.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_STORAGE_API_KEY,
+  authDomain: "ngirim-api.firebaseapp.com",
+  projectId: "ngirim-api",
+  storageBucket: "ngirim-api.appspot.com",
+  messagingSenderId: "523128938253",
+  appId: "1:523128938253:web:1c4c95b9c7bfced7c21a44",
+  measurementId: "G-7HCKQZVS0E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
---
+++ utils/getRandomNumber.ts
@@ -0,0 +1,3 @@
+export default function getRandomNumber(){
+    return [Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), Math.floor(Math.random() * 9), Math.floor(Math.random() * 9)].join('')
+}
